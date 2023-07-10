a47fbcb065b81071590cc345f89629b500c18ec3
Franklin Ngumbi Mutua <franklin.mutua@un.org>
2023-07-10 12:06:57 +0300

    startup.sh updeta

--- startup.sh
+++ startup.sh
@@ -1,9 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 
-# make sure you've copied the default file from the nginx site-enabled and edited the root directory to 
-#/home/site/wwwroot/public 
-cp /home/site/default /etc/nginx/sites-available/default
-service nginx reload
+# Navigate to the Nginx site-enabled directory
+cd /etc/nginx/sites-enabled
 
-cd /home/site/wwwroot && cp -n .env.example .env 
+# Open the default file for editing
+sed -i 's#/home/site/wwwroot#/home/site/wwwroot/public#g' default
+
+# Navigate to the Laravel application directory
+cd /home/site/wwwroot
+
+# Copy the .env.example file to .env
+cp .env.example .env
+
+# Generate a new application key for Laravel
 php artisan key:generate
+
+# Restart Nginx
+service nginx restart
